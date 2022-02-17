f7942f24ae62e8a15652ab2194e2e92967b7355e
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-02-17 18:57:07 -0500

    Finished with the mvp

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const recipesRouter = require('./recipes/recipe-router');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use('/api/recipes', recipesRouter);
 
 module.exports = app;
